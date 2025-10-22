1936015ad7988c85e2405e8afee046ec5483fa40
Casper S <csteff23@student.aau.dk>
2025-10-22 15:51:19 +0200

    Refactor to remove exclude array from mypy

--- alembic/env.py
+++ alembic/env.py
@@ -1,9 +1,9 @@
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config, pool
+from time_series.database.models import SQLModel
 
 from alembic import context
-from components.time_series.database.models import SQLModel
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
