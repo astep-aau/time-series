f58496c789b80c95e5b8ba5f05a75389d59785c7
Casper S <csteff23@student.aau.dk>
2025-11-10 10:33:51 +0100

    feat: add dataset record filtering endpoint

--- components/time_series/dataset_service/__init__.py
+++ components/time_series/dataset_service/__init__.py
@@ -1,3 +1,7 @@
-from time_series.dataset_service.overview_service import get_all_datasets, get_dataset_by_id
+from time_series.dataset_service.overview_service import (
+    get_all_datasets,
+    get_dataset_by_id,
+    get_filtered_dataset_records,
+)
 
-__all__ = ["get_all_datasets", "get_dataset_by_id"]
+__all__ = ["get_all_datasets", "get_dataset_by_id", "get_filtered_dataset_records"]
