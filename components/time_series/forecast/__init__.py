144571ebbf224e2a2fe66f682f2e1d3dacb22278
= <Fenrir.Lupum@gmail.com>
2025-11-24 11:56:24 +0100

    changed to use python types

--- components/time_series/forecast/__init__.py
+++ components/time_series/forecast/__init__.py
@@ -1,3 +1,3 @@
-from time_series.forecast.weather import get_todays_temp, get_upcomming_temps
+from time_series.forecast.weather import get_todays_temp, get_upcoming_temps
 
-__all__ = ["get_todays_temp", "get_upcomming_temps"]
+__all__ = ["get_todays_temp", "get_upcoming_temps"]
