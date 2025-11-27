9a40fb3ecde9766a7d42713824a0567298fef329
= <Fenrir.Lupum@gmail.com>
2025-11-27 10:19:59 +0100

    fix: updated to address points from meeting

--- components/time_series/forecast/weather.py
+++ components/time_series/forecast/weather.py
@@ -3,7 +3,7 @@ from typing import List, Optional, cast
 import python_weather
 
 
-async def get_todays_temp(city="London") -> Optional[int]:
+async def get_todays_temp(city) -> Optional[int]:
     try:
         client = python_weather.Client()
         weather = await client.get(city)
@@ -11,16 +11,15 @@ async def get_todays_temp(city="London") -> Optional[int]:
         await client.close()
         return weather.temperature
     except TypeError:
-        print('Given city was nil or not a string. Using default vlaue: "London"')
         await client.close()
-        return await get_todays_temp(city="London")
+        return None
     except Exception as e:
         print("unexpected exception " + str(e))
         await client.close()
         return None
 
 
-async def get_upcoming_temps(city="London") -> Optional[List[int]]:
+async def get_upcoming_temps(city) -> Optional[List[int]]:
     try:
         client = python_weather.Client()
         weather = await client.get(city)
@@ -32,9 +31,8 @@ async def get_upcoming_temps(city="London") -> Optional[List[int]]:
             ret.append(daily.temperature)
         return ret
     except TypeError:
-        print('Given city was nil or not a string. Using default vlaue: "London"')
         await client.close()
-        return await get_upcoming_temps(city="London")
+        return None
     except Exception as e:
         print("unexpected exception " + str(e))
         await client.close()
