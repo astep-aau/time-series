2d408071b08786a2a32f5560fc528c0559626b95
Sofie <sofie+git@mailbox.org>
2025-12-19 16:47:27 +0100

    chore: minor fixes to outlier_detection

--- components/time_series/outlier_detection/datasets.py
+++ components/time_series/outlier_detection/datasets.py
@@ -2,8 +2,8 @@ from typing import Any, Optional
 
 import numpy as np
 import torch
-from sqlmodel import Session, col, select
-from time_series.database import Datapoint, Dataset
+from sqlmodel import Session, select
+from time_series.database import DatapointRepository, Dataset
 
 
 class DatapointSQLDataset(torch.utils.data.Dataset):
@@ -19,32 +19,29 @@ class DatapointSQLDataset(torch.utils.data.Dataset):
         self._load_data()
 
     def _load_data(self):
-        statement = (
-            select(Datapoint.value, Datapoint.time)
-            .where(Datapoint.dataset_id == self.dataset_id)
-            .order_by(col(Datapoint.time))
-        )
-
         # Load everything to speed up learning over chunking this into a bunch of queries.
         # Database may not be locally available.
-        results = self.session.exec(statement).all()
+        datapoints = DatapointRepository(session=self.session).get_by_dataset(self.dataset_id)
 
-        if not results:
-            statement = select(True).where(Dataset.id == self.dataset_id).limit(1)
-            if not self.session.exec(statement).first():
+        # Handle empty response explicitly as an error, as we cannot continue without some data.
+        if not datapoints:
+            dataset_exists = self.session.exec(select(True).where(Dataset.id == self.dataset_id).limit(1)).first()
+            if not dataset_exists:
                 raise ValueError(f"Dataset with id={self.dataset_id} does not exist")
             raise ValueError(f"No datapoints found for dataset_id={self.dataset_id}")
 
-        self.timestamps = [r[1] for r in results]
+        # List to map actual timestamps to each time step for all values.
+        self.timestamps = [datapoint.time for datapoint in datapoints]
 
         # Normalization
-        # Log1p to handle zero and near zero values.
-        data = np.array([r[0] for r in results], dtype=np.float32)
+        values = np.array([datapoint.value for datapoint in datapoints], dtype=np.float32)
         if self.scaler is not None:
+            # Log1p to handle zero and near zero values.
             # values = np.log1p(data.reshape(-1, 1))
-            self.values = torch.tensor(self.scaler.fit_transform(data.reshape(-1, 1)).flatten(), dtype=torch.float32)
+            self.values = torch.tensor(self.scaler.fit_transform(values.reshape(-1, 1)).flatten(), dtype=torch.float32)
         else:
-            self.values = torch.tensor(data, dtype=torch.float32)
+            # Assume data is already normalized by the caller.
+            self.values = torch.tensor(values, dtype=torch.float32)
 
         self.num_sequences = max(0, (len(self.values) - self.sequence_length) // self.stride + 1)
 
