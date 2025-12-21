aff67eea8a38e58f97f28852908ccfd888770151
frederik <fredgusbach@gmail.com>
2025-12-21 19:58:16 +0100

    Added integration tests for datasets routes

---
+++ test/bases/time_series/api/conftest.py
@@ -0,0 +1,8 @@
+import os
+
+os.environ.setdefault("PORT", "8000")
+os.environ.setdefault("DATABASE_USERNAME", "test_user")
+os.environ.setdefault("DATABASE_PASSWORD", "test_password")
+os.environ.setdefault("DATABASE_HOST", "localhost")
+os.environ.setdefault("DATABASE_PORT", "5432")
+os.environ.setdefault("DATABASE_NAME", "test_db")
